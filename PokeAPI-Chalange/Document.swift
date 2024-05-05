bc4af4892ff4e48e1f5c125ce52b7c1421e743b1
mc-parch <puthiwat.p@outlier.co.th>
2024-05-05 17:04:42 +0700

    Initial Commit

---
+++ PokeAPI-Chalange/Document.swift
@@ -0,0 +1,21 @@
+//
+//  Document.swift
+//  PokeAPI-Chalange
+//
+//  Created by Mc Parch on 5/5/2567 BE.
+//
+
+import UIKit
+
+class Document: UIDocument {
+    
+    override func contents(forType typeName: String) throws -> Any {
+        // Encode your document with an instance of NSData or NSFileWrapper
+        return Data()
+    }
+    
+    override func load(fromContents contents: Any, ofType typeName: String?) throws {
+        // Load your document from contents
+    }
+}
+
